32a6163e6ec2578b9003bf7718008c1bb818d5ee
MotazHesham <motazhesham97@gmail.com>
2026-09-12 02:59:12 +0300

    Redefine semantic colors as burgundy monochrome, add hold status colors

--- lib/src/theme/color_schemes.dart
+++ lib/src/theme/color_schemes.dart
@@ -16,6 +16,10 @@ class AppColorsExtension extends ThemeExtension<AppColorsExtension> {
     this.onWarningContainer,
     this.infoContainer,
     this.onInfoContainer,
+    required this.hold,
+    required this.onHold,
+    this.holdContainer,
+    this.onHoldContainer,
   });
 
   final Color success;
@@ -30,6 +34,10 @@ class AppColorsExtension extends ThemeExtension<AppColorsExtension> {
   final Color? onWarningContainer;
   final Color? infoContainer;
   final Color? onInfoContainer;
+  final Color hold;
+  final Color onHold;
+  final Color? holdContainer;
+  final Color? onHoldContainer;
 
   @override
   ThemeExtension<AppColorsExtension> copyWith({
@@ -45,6 +53,10 @@ class AppColorsExtension extends ThemeExtension<AppColorsExtension> {
     Color? onWarningContainer,
     Color? infoContainer,
     Color? onInfoContainer,
+    Color? hold,
+    Color? onHold,
+    Color? holdContainer,
+    Color? onHoldContainer,
   }) {
     return AppColorsExtension(
       success: success ?? this.success,
@@ -59,6 +71,10 @@ class AppColorsExtension extends ThemeExtension<AppColorsExtension> {
       onWarningContainer: onWarningContainer ?? this.onWarningContainer,
       infoContainer: infoContainer ?? this.infoContainer,
       onInfoContainer: onInfoContainer ?? this.onInfoContainer,
+      hold: hold ?? this.hold,
+      onHold: onHold ?? this.onHold,
+      holdContainer: holdContainer ?? this.holdContainer,
+      onHoldContainer: onHoldContainer ?? this.onHoldContainer,
     );
   }
 
@@ -83,6 +99,10 @@ class AppColorsExtension extends ThemeExtension<AppColorsExtension> {
       onWarningContainer: Color.lerp(onWarningContainer, other.onWarningContainer, t),
       infoContainer: Color.lerp(infoContainer, other.infoContainer, t),
       onInfoContainer: Color.lerp(onInfoContainer, other.onInfoContainer, t),
+      hold: Color.lerp(hold, other.hold, t)!,
+      onHold: Color.lerp(onHold, other.onHold, t)!,
+      holdContainer: Color.lerp(holdContainer, other.holdContainer, t),
+      onHoldContainer: Color.lerp(onHoldContainer, other.onHoldContainer, t),
     );
   }
 }
@@ -92,18 +112,22 @@ class AppPalettes {
   AppPalettes._();
 
   static const light = AppColorsExtension(
-    success: Color(0xFF2E7D32),
-    onSuccess: Colors.white,
-    successContainer: Color(0xFFA5D6A7),
-    onSuccessContainer: Color(0xFF1B5E20),
-    warning: Color(0xFFED6C02),
-    onWarning: Colors.white,
-    warningContainer: Color(0xFFFFCC80),
-    onWarningContainer: Color(0xFFE65100),
-    info: Color(0xFF0288D1),
-    onInfo: Colors.white,
-    infoContainer: Color(0xFF81D4FA),
-    onInfoContainer: Color(0xFF01579B),
+    success: Color(0xFF800020),
+    onSuccess: Color(0xFFFFFFFF),
+    successContainer: Color(0xFFF7E9EC),
+    onSuccessContainer: Color(0xFF5C0016),
+    warning: Color(0xFF1A1A1A),
+    onWarning: Color(0xFFFFFFFF),
+    warningContainer: Color(0xFFEFEFEF),
+    onWarningContainer: Color(0xFF1A1A1A),
+    info: Color(0xFF1A1A1A),
+    onInfo: Color(0xFFFFFFFF),
+    infoContainer: Color(0xFFEFEFEF),
+    onInfoContainer: Color(0xFF1A1A1A),
+    hold: Color(0xFF800020),
+    onHold: Color(0xFFFFFFFF),
+    holdContainer: Color(0xFFF7E9EC),
+    onHoldContainer: Color(0xFF5C0016),
   );
 
   static const dark = AppColorsExtension(
@@ -119,8 +143,12 @@ class AppPalettes {
     onInfo: Color(0xFF01579B),
     infoContainer: Color(0xFF0277BD),
     onInfoContainer: Color(0xFFE1F5FE),
+    hold: Color(0xFF800020),
+    onHold: Color(0xFFFFFFFF),
+    holdContainer: Color(0xFFF7E9EC),
+    onHoldContainer: Color(0xFF5C0016),
   );
 }
 
 /// Access semantic colors via `context.appColors` from `context_extension.dart`.
-/// Example: `context.appColors.success`
+/// Example: `context.appColors.success`
